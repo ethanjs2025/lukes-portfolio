cdf742fd41c6883bc0b8467a39e32864fbf3daa6
ethanjs2025 <ethanjs2025@gmail.com>
2025-12-26 11:56:23 -0600

    chore: configure router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+
 function App() {
   return (
-    <>
-      <div>
-        Hello World
+    <BrowserRouter>
+      <div className="min-h-screen bg-white text-gray-900">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-    </>
+    </BrowserRouter>
   )
 }
 
